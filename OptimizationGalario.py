99008ddfa68a275985b7504a3e9f53379b9b14b6
Yohann Faure <faure.yohann@gmail.com>
2019-06-10 15:10:00 -0400

    added a classical optimization

--- OptimizationGalario.py
+++ OptimizationGalario.py
@@ -9,8 +9,9 @@ You need to adapt it to open your own file.
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import scipy
 ##### galario
-from galario.double_cuda import get_image_size, chi2Profile # computes the image size required from the (u,v) data , computes a chi2
+from galario.double import get_image_size, chi2Profile, sampleProfile # computes the image size required from the (u,v) data , computes a chi2
 from galario import deg, arcsec # for conversions
 ##### Emcee
 from emcee import EnsembleSampler
@@ -22,16 +23,18 @@ from multiprocessing import Pool
 
 
 Rmin = 1e-6  # arcsec
-dR = 0.0006    # arcsec
-nR = int(2./dR)
+dR = 0.0008    # arcsec
+nR = int(1.5/dR)
 dR *= arcsec
 Rmin*=arcsec
 ##### Define a mesh for the space
 R = np.linspace(Rmin, Rmin + dR*nR, nR, endpoint=False)
 u, v, Re, Im, w = np.require(np.loadtxt('uvtable2.txt', unpack=True), requirements='C')
+
+
+#x=np.random.rand(len(uf))<0.01
+
 nxy, dxy = get_image_size(u, v, verbose=False)
-
-
 #lastsamples,_,_,_=np.load('results/optimization/optigal_6_60_6300_merged.npy',allow_pickle=True)
 #lastsamples=lastsamples[:,-500:,:].reshape((-1,6))
 
@@ -66,8 +69,8 @@ def PowerGaussianRing(i0,sig,gam,center):
 
 
 ##### Just to define which to multiply by arcsec and which to power10
-power10=np.array([0,2,6,9,12])
-ListOfParams=np.arange(0,15,1)
+power10=np.array([0,2,5,8,11])
+ListOfParams=np.arange(0,14,1)
 mask = np.ones(ListOfParams.shape,dtype=bool)
 mask[power10]=np.zeros(power10.shape)
 timesarcsec=ListOfParams[mask]
@@ -79,49 +82,19 @@ def pre_conversion(p):
     return(pout)
 
 def ModelJ1615(p):
-    f0_0, sigma_0,i0_1,sig_1,gam_1,center_1,amplitude_2, width_2, center_2,amplitude_3, width_3, center_3,amplitude_4, width_4, center_4=pre_conversion(p)
+    f0_0, sigma_0,i0_1,sig_1,center_1,amplitude_2, width_2, center_2,amplitude_3, width_3, center_3,amplitude_4, width_4, center_4=pre_conversion(p)
     return(GaussianProfile(f0_0, sigma_0)+
-        PowerGaussianRing(i0_1,sig_1,gam_1,center_1)+
+        #PowerGaussianRing(i0_1,sig_1,gam_1,center_1)+
+        GaussianRing(i0_1, sig_1, center_1)+
         GaussianRing(amplitude_2, width_2, center_2)+
         GaussianRing(amplitude_3, width_3, center_3)+
         GaussianRing(amplitude_4, width_4, center_4))
 
-#p0 = np.array([igauss*0.97,0.02,
-#    igauss*.98,0.1,3.,0.12,
-#    igauss*.95,.5,0.15,
-#    igauss*.93,.05,.5,
-#    igauss*.93,.05,.7])
-
 labels=['f0_0', 'sigma_0',
-        'i0_1','sig_1','gam_1','center_1',
+        'i0_1','sig_1', 'center_1',
         'amplitude_2', 'width_2', 'center_2',
         'amplitude_3', 'width_3', 'center_3',
         'amplitude_4', 'width_4', 'center_4']
-
-#p_range=np.transpose(np.array((p0/100,p0*100)))
-
-p_range=np.array([
-    [8.,15.],
-        [-0.,0.02],#
-    [8.,12.],
-        [0.05,0.15],
-        [0.1,5.],
-        [-0.,0.4],#
-    [1.,20.],
-        [0.1,1.],
-        [-0.,0.3],#
-    [5.,15.],
-        [0.01,0.3],
-        [0.4,1.2],#
-    [2.,12.],
-        [-0.,0.1],
-        [-0.,1.2]
-    ])
-
-
-p0=np.mean(p_range,axis=1)
-#plt.plot(f);plt.plot(ModelJ1615(p0));plt.show()
-
 
 ##### define the cost functions
 def lnpriorfn(p):
@@ -139,17 +112,82 @@ def lnpostfn(p):
     chi2 = chi2Profile(f, Rmin, dR, nxy, dxy, u, v, Re, Im, w, inc=inc, PA=PA, dRA=dRA, dDec=dDec)
     return(-0.5 * chi2)
 
+def chi2compute(ModelVal,Re,Im,w):
+    return( np.sum( ((np.imag(ModelVal)-Im)**2.+(np.real(ModelVal)-Re)**2.)*w  ) )
 
-nwalkers=150
-ndim=15
-nthreads=4
-iterations=100
 
-pos = np.array([(1. + 1.e-1*np.random.random(ndim))*p0 for i in range(nwalkers)])
+def lnpostfnbis(p):
+    """ Log of posterior probability function """
+    lnp = lnpriorfn(p)
+    if not np.isfinite(lnp):
+        return(-np.inf)
+    # compute the model brightness profile
+    f = ModelJ1615(p)
+    ModelVal=sampleProfile(f,Rmin,dR,nxy,dxy,u,v)
+    chi2 = chi2compute(ModelVal,Re,Im,w)
+    return(-0.5 * chi2)
+
+def tominimize(p):
+    """ Log of posterior probability function """
+    lnp = lnpriorfn(p)
+    if not np.isfinite(lnp):
+        return(-np.inf)
+    # compute the model brightness profile
+    f = ModelJ1615(p)
+    ModelVal=sampleProfile(f,Rmin,dR,nxy,dxy,u,v)
+    chi2 = chi2compute(ModelVal,Re,Im,w)
+    return(chi2)
+
+p0=[10.85096006,  0.01146675,
+    10.35811935,  0.09957152,  0.15475334,
+    9.85595757,  0.38882761,  0.24746409,
+    8.44414471,  0.06197724,  0.52520985,
+    8.4625868 ,  0.18377439, 0.55165571]
+
+
+p_range=np.array([
+    [8.,15.],
+        [-0.,0.02],#
+    [10.,11.],
+        [0.05,0.15],
+    #    [0.1,5.],
+        [0.1,0.2],#
+    [9.,11.],
+        [0.3,0.5],
+        [0.2,0.3],#
+    [8.,9.],
+        [0.01,0.25],
+        [0.4,0.7],#
+    [8.,9.],
+        [0.1,0.25],
+        [0.4,0.7]
+    ])
+
+
+#a=scipy.optimize.minimize(tominimize,p0,method='Nelder-Mead')
+#a=scipy.optimize.minimize(tominimize,a['x'],method='Nelder-Mead')
+#a=scipy.optimize.minimize(tominimize,p0,method='Powell')
+#a=scipy.optimize.minimize(tominimize,p0,method='CG')
+#a=scipy.optimize.minimize(tominimize,p0,method='BFGS',bounds=p_range)
+
+#p0=a[x]
+
+nwalkers=140
+ndim=14
+nthreads=20
+iterations=2000
+
+pos = np.array([(1. + 1.e-2*np.random.random(ndim))*p0 for i in range(nwalkers)])
+
+
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
 
 with Pool(processes=nthreads) as pool:
-    sampler = EnsembleSampler(nwalkers, ndim, lnpostfn,pool=pool)
+    sampler = EnsembleSampler(nwalkers, ndim, lnpostfnbis,pool=pool)
     pos, prob, state = sampler.run_mcmc(pos, iterations, progress=True)
+
+samples=sampler.chain
 
 np.save('results/optimization/optigal_{}_{}_{}'.format(ndim,nwalkers,iterations),(samples,p_range[:,0],p_range[:,1],labels))
 
